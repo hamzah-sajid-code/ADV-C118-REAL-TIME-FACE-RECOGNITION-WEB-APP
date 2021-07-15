3a8f6a97b95f817465728f3d1230a1370acb4d97
hamzah-sajid-code <82176013+hamzah-sajid-code@users.noreply.github.com>
2021-07-15 18:21:52 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,25 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide();
     video.size(400, 400);
+    classifire = ml5.imageClassifier('https://storage.googleapis.com/tm-model/EqyTYJiBe/model.json', modelLoaded);
 }
 function draw(){
-    image(video, 0, 0, 400, 400)
+    image(video, 0, 0, 400, 400);
+    classifire.classify(video, gotResult);
+}
+function modelLoaded(){
+    console.log('Successfully loaded the model')
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+        object_name = document.getElementById('object_name');
+        accuracy_number = document.getElementById('accuracy_number');
+        mainResultOfName = results[0].label;
+        mainResultOfAccuracy = results[0].confidence;
+        object_name.innerHTML = mainResultOfName;
+        accuracy_number.innerHTML = mainResultOfAccuracy.toFixed(3);
+    }
 }
